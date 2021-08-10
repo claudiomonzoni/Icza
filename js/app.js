f6fd97b162e23165be13f5199805fcbdd8832ffd
claudio Monzoni <claudiomonzoni@hotmail.com>
2021-08-09 19:07:04 -0500

    agregado el rest api

--- js/app.js
+++ js/app.js
@@ -1,8 +1,67 @@
-const cargando = document.getElementById("cargando")
-window.onload = function(){
-cargando.classList.add("eliminado")
+import { slideIndex } from "./glide";
+import gsap from "gsap";
+const cargando = document.getElementById("cargando");
+window.onload = function () {
+  cargando.classList.add("eliminado");
+};
 
-}
+// importo gsap
+// // llamo gspa
+gsap.from(".logo, ul>li", {
+  stagger: 0.1,
+  x: 400,
+  opacity: 0,
+  duration: 1, //1 segundo
+});
+
+// cargar api rest
+const ulslide = document.querySelector(".glide__slides");
+
+const cargaBanners = () => {
+  fetch(
+    "http://localhost/icza/api/collections/get/Banners/posts?token=19699268c383fc6d78989e43405c7a",
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((banners) => {
+      const losbanners = banners.entries;
+      losbanners.forEach((banner) => {
+        if(!banner.liga==""){
+          const li = `  
+          <li class="glide__slide">
+          <div class="bandeja">
+            <h1>
+             ${banner.Texto}
+            </h1>
+            <a href="${banner.liga}" class="btn">${banner.txt_btn}</a>
+          </div>
+          <img src="http://localhost/${banner.fondo_banner.path}" alt=" ${banner.Texto}" />
+        </li>`;
+        ulslide.innerHTML += li;
+        }else{
+          const li = `  
+          <li class="glide__slide">
+          <div class="bandeja">
+            <h1>
+             ${banner.Texto}
+            </h1>
+          </div>
+          <img src="http://localhost/${banner.fondo_banner.path}" alt=" ${banner.Texto}" />
+        </li>`;
+        ulslide.innerHTML += li;
+        }
+   
+      });
+    })
+    .then(() => {
+      //llamo el carrusel
+      slideIndex()
+      console.log("2");
+    });
+};
+//cargaBanners();
+
 //only phones
 var x = window.matchMedia("(max-width: 479px)");
 esCell(x); // Call listener function at run time
@@ -18,7 +77,7 @@ function esCell(x) {
     // const anchor = document.querySelector(".anchor");
     const abierto = false;
 
-    function abrirMenuFull () {
+    function abrirMenuFull() {
       this.abierto = !this.abierto;
       mainNav.classList.remove("menuFullAbierto");
       mainNav.classList.remove("desanimaMenu");
@@ -27,7 +86,7 @@ function esCell(x) {
       this.abierto
         ? mainNav.classList.toggle("menuFullAbierto")
         : mainNav.classList.toggle("desanimaMenu");
-    };
+    }
 
     menuIco.addEventListener("click", abrirMenuFull);
     //anchor.addEventListener("click", abrirMenuFull);
@@ -36,49 +95,35 @@ function esCell(x) {
   }
 }
 
-// insertar iconos de redes sociales 
-import Icono, {face, insta} from './iconos'
-const faceicono = new Icono("http://facebook.com", "#social", face)
-faceicono.construirIcono()
-const instaIcono = new Icono("http://instagram.com", "#social", insta)
-instaIcono.construirIcono()
-
-//importo el carrusel
-import {slideIndex} from './glide'
-slideIndex()
-
-// importo gsap
-import gsap from 'gsap'
-gsap.from(".logo, ul>li",
-{
-  stagger:0.1,
-  x:400,
-  opacity: 0,
-  duration: 1 //1 segundo
-})
+// insertar iconos de redes sociales
+import Icono, { face, insta } from "./iconos";
+const faceicono = new Icono("http://facebook.com", "#social", face);
+faceicono.construirIcono();
+const instaIcono = new Icono("http://instagram.com", "#social", insta);
+instaIcono.construirIcono();
 
 //menu efecto al hacer scroll
-const body = document.getElementById('nav');
+const body = document.getElementById("nav");
 let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
-	const currentScroll = window.pageYOffset;
-	if (currentScroll <= 0) {
-		body.classList.remove("scroll-up");
-		return;
-	}
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+    return;
+  }
 
-	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-		body.classList.remove("scroll-up");
-		body.classList.add("scroll-down");
-	} else if (
-		currentScroll < lastScroll &&
-		body.classList.contains("scroll-down")
-	) {
-		body.classList.remove("scroll-down");
-		body.classList.add("scroll-up");
-	}
-	lastScroll = currentScroll;
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains("scroll-down")
+  ) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+  lastScroll = currentScroll;
 });
 
 const WOW = require("wowjs");
@@ -87,13 +132,3 @@ window.wow = new WOW.WOW({
   live: false,
 });
 window.wow.init();
-
-
-// cargar modulos solo por secciones
-// switch (document.location.pathname)
-// {
-//   case '/seccion':
-//   break
-
-//   default:
-// }
--- js/glide.js
+++ js/glide.js
@@ -1,25 +1,22 @@
-import Glide from '@glidejs/glide'
-import { Controls } from '@glidejs/glide/dist/glide.modular.esm'
+import Glide from "@glidejs/glide";
+import { Controls } from "@glidejs/glide/dist/glide.modular.esm";
 
-console.log(Controls)
-export function slideIndex(){
-new Glide('.glide',{
-    type: 'carousel',
-  startAt: 0,
-  perView: 1, 
-  focusAt: 'center',
-  animationDuration: 2500,
-  // breakpoints: {
-  //   1024: {
-  //     perView: 1
-  //   },
-  //   600: {
-  //     perView: 1
-  //   }
-  // },
-  autoplay: 6000
-}).mount(
-  {Controls}
-)
-
+console.log(Controls);
+export function slideIndex() {
+  new Glide(".glide", {
+    type: "carousel",
+    startAt: 0,
+    perView: 1,
+    focusAt: "center",
+    animationDuration: 2500,
+    // breakpoints: {
+    //   1024: {
+    //     perView: 1
+    //   },
+    //   600: {
+    //     perView: 1
+    //   }
+    // },
+    autoplay: 5000,
+  }).mount({ Controls });
 }
