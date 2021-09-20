3cc6088e8f2960352c086ca8fc3ff365ac3684d0
claudio Monzoni <claudiomonzoni@hotmail.com>
2021-09-20 11:56:07 -0500

    agragando clientes

--- js/leaflet.js
+++ js/leaflet.js
@@ -11,7 +11,7 @@ export class Mapa {
     const map = L.map('mapa',{
       scrollWheelZoom:false,
       zoomOffset: -1
-    }).setView([17.65003, -101.533493], 16)
+    }).setView([20.694448, -103.375751], 16)
     const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>'
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; ' + enlaceMapa + ' Contributors',
@@ -25,10 +25,10 @@ export class Mapa {
     const pines = {
       resultados: [
         {
-          latitude: '17.650030',
-          longitude: '-101.533493',
-          titulo: 'Instituto Lizardi',
-          calle: 'Calle Ares, Col. El Hujal, Zihuatanejo Gro. México.',
+          latitude: '20.694448',
+          longitude: '-103.375751',
+          titulo: 'Icza',
+          calle: 'Avenida Adolfo Lopez Mateos Norte 95 Italia Providencia, Guadalajara, Jalisco 44648',
         },
       ],
     }
@@ -52,7 +52,7 @@ export class Mapa {
       // objeto de los markers
       const elpin = L.icon({
         iconUrl:
-          'https://www.equirentazihua.com/wp-content/themes/equirenta/img/mapapin.svg',
+          'https://www.idmtest.com/style/img/ic-marker.svg',
         //shadowUrl: "leaf-shadow.png",
 
         iconSize: [50, 69], // size of the icon
